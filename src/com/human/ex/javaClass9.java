e542403759cf8c4549773e082e80558641d23040
Administrator <Administrator@192.168.0.84>
2020-02-21 17:00:38 +0900

    asdasd

---
+++ src/com/human/ex/javaClass9.java
@@ -0,0 +1,24 @@
+package com.human.ex;
+
+public class javaClass9 {
+
+	public static void main(String[] args) {
+		java.util.Scanner sc = new java.util.Scanner(System.in);
+		System.out.println("°ü°´¼ö´Â:");
+		String str = sc.nextLine();
+		int i1 = Integer.parseInt(str);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
